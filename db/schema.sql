be444e38274be033cdd4662270b7aebf64b769d5
Joe Porrazzo <joepa2624@me.com>
2021-06-06 21:49:52 -0700

    add employee function

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,25 @@ DROP TABLE IF EXISTS employees;
 
 
 CREATE TABLE departments (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	department_name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30) NOT NULL,
-    salary INTEGER NOT NULL,
-    department_id INTEGER NOT NULL,
+    salary INT NOT NULL,
+    department_id INT NOT NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
-	role_id INTEGER,
-	manager_id INTEGER,
-	CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+	role_id INT,
+	manager_id INT,
+	FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
 
 
